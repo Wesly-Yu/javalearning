ed1f0ea022caebb57e40e16fd11e928d616d4c81
Wesly-Yu <1633235633@qq.com>
2020-08-19 21:40:18 +0800

    static method  02

---
+++ src/main/java/baseapi/Day04/MyClass.java
@@ -0,0 +1,17 @@
+package baseapi.Day04;
+
+public class MyClass {
+    int num;
+    static int numStatic;
+    public  void method(){
+        System.out.println("这是一个成员方法");
+        System.out.println(num);
+        System.out.println(numStatic);
+    }
+    public static void staticmethod(){
+        System.out.println("这是一个静态方法");
+        System.out.println(numStatic);
+        System.out.println(num);//静态方法无法访问 非静态成员变量
+
+    }
+}
